c0c2c3fdf99640ce887a2e3c9b8599270aeaf6b7
mayi-mama <mayimuro8845@gmail.com>
2023-07-15 11:15:58 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer class="site-footer">
+        <p class="copyright">&copy; <?php bloginfo('name'); ?></p>
+    </footer>
+
+    <!-- Script -->
+    <?php wp_footer() ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+    <main class="main-contents wrapper">
+        <div class="post-list">
+            <?php if(have_posts()): while(have_posts()); the_post(); ?>
+            <article class="post-item">
+                <a href="<?php echo esc_url(get_permalink()); ?>">
+                    <img src="images/img-fruits.jpg" alt="" class="wp-post-image">
+                </a>
+                <header class="post-header">
+                    <h2 class="post-title">
+                        <a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title(); ?></a>
+                    </h2>
+                    <time class="post-date" datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php the_date(); ?></time>
+                    <?php the_category(); ?>
+                </header>
+            </article>
+            <?php endwhile; esle : ?>
+              
+            <?php endif; ?>
+        </div>
+
+        <div class="nav-links">
+            <a href="#">← 新しい投稿</a>
+            <a href="#">過去の投稿 →</a>
+        </div>
+    </main>
+
+    <?php get_footer(); ?>
